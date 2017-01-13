859d45a9d151043a7309f1744b55eba623fb50be
Duc Dang <minhduc250991@gmail.com>
2017-01-13 17:48:40 -0600

    Delete testing.m

--- Matlab code/Project 1/testing.m
+++
@@ -1,7 +0,0 @@
-clc; close all; clear all;
-[orig, patch_info] = generate_chart (320, 480, 24, 16, 240, 16, 240, 16, 240, 8, 16);
-
-%prog = orig;
-%save('patch_info.mat','patch_info');
-
-%[cal_img, a, fit] = color_xform(orig, 1, 1, 480, 320, patch_info, prog, 1, 1, 480, 320, 0, -1);
